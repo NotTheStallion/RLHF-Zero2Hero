446b9e5af324baf815359b9a9ea30580c715e4f6
The_Stallion <111058733+NotTheStallion@users.noreply.github.com>
2025-05-06 15:03:48 +0200

    Adapting shell script to PLAFRIM : [M] "train_rm_llama_.sh"

--- train_rm_llama_.sh
+++ train_rm_llama_.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
 set -x
 
+script_content=$(cat <<'EOF'
 python3 train_rm_.py \
     --save_path ./checkpoint/llama3-8b-rm \
     --save_steps -1 \
@@ -19,3 +21,35 @@ python3 train_rm_.py \
     --load_checkpoint \
     --gradient_checkpointing \
     --max_samples 100
+EOF
+)
+
+if [[ $1 == "sbatch" ]]; then
+    # Job name
+    #SBATCH -J TEST_Slurm
+    # Asking for one node
+    #SBATCH -N 1
+    #SBATCH -n 4
+    # Standard output
+    #SBATCH -o slurm.sh%j.out
+    # Standard error
+    #SBATCH -e slurm.sh%j.err
+
+    echo "=====my job information ===="
+    echo "Node List: " $SLURM_NODELIST
+    echo "my jobID: " $SLURM_JOB_ID
+    echo "Partition: " $SLURM_JOB_PARTITION
+    echo "submit directory:" $SLURM_SUBMIT_DIR
+    echo "submit host:" $SLURM_SUBMIT_HOST
+    echo "In the directory:" $PWD
+    echo "As the user:" $USER
+
+    # module purge
+    # module load compiler/gcc
+    module load compiler/cuda
+    module load language/python/3.10
+    pip install -r requirements.txt
+    bash -c "srun -n4 -C sirocco&v100 ${script_content}"
+else
+    bash -c "${script_content}"
+fi
